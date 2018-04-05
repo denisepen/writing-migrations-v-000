4d525b65715620747c3fb88cb48fa6dac2a49f0d
Denise Pendleton <dpendl1@hotmail.com>
2018-04-05 15:20:44 +0000

    automatically backed up by learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,6 +1,8 @@
-class AddGradeAndBirthdateToArtist < ActiveRecord::Migration[5.1]
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
 
   def change
+    add_column :students, :grade, :integer
+    add_column :students, :birthdate, :string
 
   end
 
